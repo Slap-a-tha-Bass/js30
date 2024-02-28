33c31d595fd7dbfb0c40929fddc6ee0a25edf7dc
Corey DeLoach <cdeloach16@gmail.com>
2024-02-28 13:01:09 -0500

    linking css and script files, basic styling to match

---
+++ day1/index.js
@@ -0,0 +1,4 @@
+// each key (A, S, D, F, G, H, J, K, L) is a note that brings in a audio file
+// whenever each key is pressed, the sound is played and the border changes color and the whole div is scaled up larger
+
+console.log("Hello World");
