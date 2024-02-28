7b0a796dd7ec77ce36f724248e96e6822d4ece0f
Corey DeLoach <cdeloach16@gmail.com>
2024-02-28 15:07:42 -0500

    adding event listeners, copying over audio files, switch case for all of the key events

--- day1/index.js
+++ day1/index.js
@@ -1,4 +1,63 @@
 // each key (A, S, D, F, G, H, J, K, L) is a note that brings in a audio file
 // whenever each key is pressed, the sound is played and the border changes color and the whole div is scaled up larger
 
-console.log("Hello World");
+const keys = document.querySelectorAll(".key");
+
+window.addEventListener("keydown", function (e) {
+  keys.forEach((key) => {
+    key.classList.remove("highlight");
+    if (key.textContent.toUpperCase() === e.key.toUpperCase()) {
+      key.classList.add("highlight");
+      setTimeout(() => {
+        key.classList.remove("highlight");
+      }, 250);
+    }
+  });
+  switch (e.key) {
+    case "a":
+      console.log("A");
+      const audio_a = new Audio("sounds/a.wav");
+      audio_a.play();
+      break;
+    case "s":
+      console.log("S");
+      const audio_s = new Audio("sounds/s.wav");
+      audio_s.play();
+      break;
+    case "d":
+      console.log("D");
+      const audio_d = new Audio("sounds/d.wav");
+      audio_d.play();
+      break;
+    case "f":
+      console.log("F");
+      const audio_f = new Audio("sounds/f.wav");
+      audio_f.play();
+      break;
+    case "g":
+      console.log("G");
+      const audio_g = new Audio("sounds/g.wav");
+      audio_g.play();
+      break;
+    case "h":
+      console.log("H");
+      const audio_h = new Audio("sounds/h.wav");
+      audio_h.play();
+      break;
+    case "j":
+      console.log("J");
+      const audio_j = new Audio("sounds/j.wav");
+      audio_j.play();
+      break;
+    case "k":
+      console.log("K");
+      const audio_k = new Audio("sounds/k.wav");
+      audio_k.play();
+      break;
+    case "l":
+      console.log("L");
+      const audio_l = new Audio("sounds/l.wav");
+      audio_l.play();
+      break;
+  }
+});
